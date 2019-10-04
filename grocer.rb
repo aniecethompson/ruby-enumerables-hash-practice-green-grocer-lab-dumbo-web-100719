516a48a360b09c28fd0feb409848e022c6dc7e00
Aniece Thompson <aniecet@yahoo.com>
2019-10-04 13:06:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -27,26 +27,6 @@ end
 # cart
 # end
 
-def apply_single_coupon(cart, coupon)
-
-  coupItem = coupon[:item]
-
-  if cart.keys.include?(coupItem) && cart[coupItem][:count] >= coupon[:num]
-    cart[coupItem][:count] = cart[coupItem][:count] - coupon[:num]
-    cart["#{coupItem} W/COUPON"] ||= {}
-    cart["#{coupItem} W/COUPON"][:price] = coupon[:cost]
-    cart["#{coupItem} W/COUPON"][:clearance] = cart[coupItem][:clearance]
-    cart["#{coupItem} W/COUPON"][:count] ||= 0
-    cart["#{coupItem} W/COUPON"][:count] += 1
-  end
-end
-
-def apply_coupons(cart: [], coupons: [])
-    coupons.each do |couponHash|
-    apply_single_coupon(cart, couponHash)
-  end
-  cart
-end
 
 
      

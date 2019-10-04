e6078a1acd0781425e3d8f7b019e80fcb4253478
Aniece Thompson <aniecet@yahoo.com>
2019-10-04 13:08:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -28,8 +28,8 @@ end
 # end
 
 def apply_coupons(cart:[], coupons:[])
-  # code here	  # code here
-  # cart_cons = consolidate_cart(cart: cart)
+
+  cart_cons = consolidate_cart(cart: cart)
   cart_cons = cart
   coupons.each do |coupon|
     item_name = coupon[:item]

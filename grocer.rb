22e926a905bc12022a9872f7e93b6e9d05aa294a
Aniece Thompson <aniecet@yahoo.com>
2019-10-04 12:54:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -27,25 +27,25 @@ end
 # cart
 # end
 
-def apply_coupons(cart, coupons) 
-  
-  coupons.each do |coupon| 
-    coupon.each do |attribute, value| 
-      name = coupon[:item] 
+def apply_coupons(cart, coupons)
+  coupons.each do |coupon_hash|
+    fruit_name = coupon_hash[:item]
+    new_coupon_hash = {
+      :price => coupon_hash[:cost],
+      :clearance => "true",
+      :count => coupon_hash[:num]
+    }
     
-      if cart[name] && cart[name][:count] >= coupon[:num] 
-        if cart["#{name} W/COUPON"] 
-          cart["#{name} W/COUPON"][:count] += 1 
-        else 
-          cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
-          :clearance => cart[name][:clearance], :count => 1} 
-        end 
-  
-      cart[name][:count] -= coupon[:num] 
-    end 
-  end 
-end 
-  cart 
+     if cart.key?(fruit_name)
+      new_coupon_hash[:clearance] = cart[fruit_name][:clearance]
+      if cart[fruit_name][:count]>= new_coupon_hash[:count]
+        new_coupon_hash[:count] = (cart[fruit_name][:count]/new_coupon_hash[:count]).floor
+        cart[fruit_name][:count] = (coupon_hash[:num])%(cart[fruit_name][:count])
+      end
+      cart[fruit_name + " W/COUPON"] = new_coupon_hash 
+    end
+    end
+  return cart
 end
 
      

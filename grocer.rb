1bf1b8c126595f1790d88e65d564b648220d42ef
Aniece Thompson <aniecet@yahoo.com>
2019-10-04 12:37:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,71 @@
+
 def consolidate_cart(cart)
-  # code here
+    new_cart = {} 
+    cart.each do |items| 
+      items.each do |item, new_pair| 
+        new_cart[item] ||= new_pair 
+        new_cart[item][:count] ? new_cart[item][:count] += 1 :   
+        new_cart[item][:count] = 1 
+      end 
+    end 
+    new_cart 
 end
+
+# def apply_coupons(cart, coupons)
+# coupons.each do |coupon|
+# item = coupon[:item] 
+# if cart.has_key?("item")
+# if cart[item][count] >= coupon[:num] && !cart.has_key?("#{item} W/COUPON")
+# cart["#{item} W/COUPON"] = {price:coupon[:cost]/ coupon[num], clearance: cart[item][:clearance], count:coupon[:num]}
+
+# elsif cart[item][count] >= coupon[:num] && cart.has_key?("#{item} W/COUPON")
+# cart["#{item} W/COUPON"][:count] += coupon[:num]
+# end
+# cart[item][:count] -= coupon[:num]
+# end
+# end
+# cart
+# end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |element|
+    name= element[:item]
+    amount= element[:num]
+    
+    if cart.has_key?(name) && cart[name][:count] >= element[:num] && cart[name][:clearance]
+      puts "yes"
+    end
+    
+  end
+  
 end
 
+     
 def apply_clearance(cart)
-  # code here
+  new_cart = cart
+  cart.each do |name, hash|
+      if hash[:clearance] 
+        new_cart[name][:price] = (cart[name][:price] * 0.8).round(2)
+      end
+  end
+  new_cart
 end
+
+
 
 def checkout(cart, coupons)
-  # code here
+  new_cart = consolidate_cart(cart)
+  apply_coupons(new_cart, coupons)
+  apply_clearance(new_cart)
+  total = 0
+  new_cart.each do |name, hash|
+    total += (hash[:price] * hash[:count])
+  end
+
+  if total >= 100
+      total *= 0.9
+  end
+  total
 end
+
+  
